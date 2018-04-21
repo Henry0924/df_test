75be79158b6b3ed348bfcb9bff18c4e672fbc3b6
Henry <13477096357@163.com>
2018-04-21 22:35:42 +0800

    首页显示，fastDFS

--- apps/goods/urls.py
+++ apps/goods/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import include, url
 from apps.goods import views
+from apps.goods.views import IndexView
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', IndexView.as_view(), name='index'),
     url(r'^list$', views.goods_list, name='list'),
     url(r'^detail$', views.detail, name='detail'),
 ]
--- apps/goods/views.py
+++ apps/goods/views.py
@@ -1,7 +1,44 @@
 from django.shortcuts import render
+from django.views.generic import View
+from apps.goods.models import GoodsType, IndexGoodsBanner, IndexTypeGoodsBanner, IndexPromotionBanner
+from django_redis import get_redis_connection
 
 
 # Create your views here.
+class IndexView(View):
+    def get(self, request):
+        """显示首页"""
+        # 1.获取所有商品种类
+        types = GoodsType.objects.all()
+        # 2.获取首页轮播图片
+        goods_banner = IndexGoodsBanner.objects.all().order_by('index')
+        # 3.获取首页广告图片
+        promotion_banner = IndexPromotionBanner.objects.all().order_by('index')
+        # 4.获取每个种类的商品图片和文字信息
+        for type in types:
+            # 获取每个种类的商品图片信息
+            goods_imgs = IndexTypeGoodsBanner.objects.filter(type=type, display_type=1)
+            # 获取每个种类的商品文字信息
+            goods_names = IndexTypeGoodsBanner.objects.filter(type=type, display_type=0)
+
+            type.goods_imgs = goods_imgs
+            type.goods_names = goods_names
+        # 5.获取购物车的数目
+        cart_count = 0
+        if request.user.is_authenticated():
+            conn = get_redis_connection('default')
+            cart_key = 'cart_%d' % request.user.id
+            cart_count = conn.hlen(cart_key)
+
+        # 准备上下文模板
+        content = {'types': types,
+                   'goods_banner': goods_banner,
+                   'promotion_banner': promotion_banner,
+                   'cart_count': cart_count}
+        # 使用上下文模板
+        return render(request, 'goods/index.html', content)
+
+
 def index(request):
     return render(request, 'goods/index.html')
 
